2705674acab21d80906c24ef6d87525ba09a7e16
SEUNGJ0 <you@example.com>
2023-05-09 15:32:10 +0900

    5월9일 실시간 무게 측정 방식

--- start.py
+++ start.py
@@ -15,23 +15,32 @@ def feed_output(amount):
     lcd.lcd_init() 
     feedmass = Massdetection()
     lcd.lcd_string(f"Target : {amount}g.", LCD_LINE_1)
-    i = 0
-    is_running = True
-    def run_motor():
-        while is_running:
-            STM.run_with_command(is_running)
-            time.sleep(0.002)
-        STM.run_with_command(is_running)
-    t = threading.Thread(target=run_motor)
-    t.start()
+
+    STM.run_on()
     while(True):
         lcd.lcd_string(f"Measured : {feedmass.weight_input()}g", LCD_LINE_2)
         time.sleep(0.1)
         if feedmass.weight_input() >= amount:
-            is_running = False
-            t.join()
+            STM.run_off()
             print(feedmass.weight_input())
             return feedmass.weight_input()
+
+def time_add(time_str_list):
+    # 시간과 분을 분리하고 정수형으로 변환
+    new_time_str_list = []
+    for time_str in time_str_list:
+        hours, minutes = map(int, time_str.split(':'))
+
+        # 30분 추가
+        hours, minutes = divmod(hours*60 + minutes + 30, 60)
+
+        # 시간을 24로 나눈 나머지로 설정
+        hours %= 24
+
+        # 새로운 시간을 문자열로 출력
+        new_time_str = f"{hours:02d}:{minutes:02d}"
+        new_time_str_list.append(new_time_str)
+    return new_time_str_list
 
 def main():
     # LCD 초기화
@@ -39,15 +48,15 @@ def main():
     while True:
         # 사용자가 설정한 펫 정보데이터와 하루 지급 사료량 읽어옴
         amount, pet_data = load_pet_info()
-
         CrtTime = time.strftime('%H:%M')
         
         # 현재 시각과 지정된 시간 대조 조건문 
-        pet_data_list = [pet_data['pet_feed_time_B'], pet_data['pet_feed_time_L'], pet_data['pet_feed_time_D']]
+        feed_time_list = [pet_data['pet_feed_time_B'], pet_data['pet_feed_time_L'], pet_data['pet_feed_time_D']]
+        feed_time_add_list = time_add(feed_time_list)
         epf_args_list = [0, 0, 0, 0, 0, 0]
         today_feed, feed_data = read_today_data()
-        if CrtTime in pet_data_list:
-            feed_index = int(pet_data_list.index(CrtTime))
+        if CrtTime in feed_time_list:
+            feed_index = int(feed_time_list.index(CrtTime))
             if not today_feed['feedings'][feed_index]["feed_index"]:
                 given_amount = feed_output(amount)
                 lcd.lcd_string("Done!!", LCD_LINE_1)
@@ -55,7 +64,7 @@ def main():
                 epf_args_list[feed_index+3] = int(given_amount)
                 edit_pet_feed(epf_args_list)
                 time.sleep(5)
-
+                                
         else:
             lcd.lcd_string("Current Time", LCD_LINE_1)
             lcd.lcd_string(CrtTime, LCD_LINE_2)
@@ -67,3 +76,4 @@ if __name__ == "__main__":
     process = multiprocessing.Process(target=WSC_main)
     process.start()
     main()
+
